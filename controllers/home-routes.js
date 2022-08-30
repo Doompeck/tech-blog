e02e3c06029005606039e0b777d7768f3adfae1f
Benjamin <bstanf@uw.edu>
2022-08-29 18:29:45 -0700

    built out structure and began adding in pseudo-code

---
+++ controllers/home-routes.js
@@ -0,0 +1,15 @@
+// Assignment 20 is a good step in the right direction
+
+// These are the client side routes.
+
+// Route "/"
+
+// Route "/login"
+
+// Route "/dashboard"
+
+// Route "/dashboard/new"
+
+// Route "/dashboard/edit/:id"
+
+// Route "/post/:id"
